5ba8694619164ff7e57c2abbfcbd44c3466251f6
mrdotx <klassiker@gmx.de>
2020-01-14 15:35:46 +0100

    maintenance

--- config.def.h
+++ config.def.h
@@ -2,16 +2,16 @@
 
 static char *fontfallbacks[] = {
     "source code pro",
-	"dejavu sans",
-	"roboto",
-	"ubuntu",
+    "dejavu sans",
+    "roboto",
+    "ubuntu",
 };
 #define NUMFONTSCALES 42
 #define FONTSZ(x) ((int)(10.0 * powf(1.1288, (x)))) /* x in [0, NUMFONTSCALES-1] */
 
 static const char *colors[] = {
-	"#FFFFFF", /* foreground color */
-	"#000000", /* background color */
+    "#FFFFFF", /* foreground color */
+    "#000000", /* background color */
 };
 
 static const float linespacing = 1.0;
@@ -21,37 +21,37 @@ static const float usablewidth = 1.00;
 static const float usableheight = 1.00;
 
 static Mousekey mshortcuts[] = {
-	/* button         function        argument */
-	{ Button1,        advance,        {.i = +1} },
-	{ Button3,        advance,        {.i = -1} },
-	{ Button4,        advance,        {.i = -1} },
-	{ Button5,        advance,        {.i = +1} },
+    /* button         function        argument */
+    { Button1,        advance,        {.i = +1} },
+    { Button3,        advance,        {.i = -1} },
+    { Button4,        advance,        {.i = -1} },
+    { Button5,        advance,        {.i = +1} },
 };
 
 static Shortcut shortcuts[] = {
-	/* keysym         function        argument */
-	{ XK_Escape,      quit,           {0} },
-	{ XK_q,           quit,           {0} },
-	{ XK_Right,       advance,        {.i = +1} },
-	{ XK_Left,        advance,        {.i = -1} },
-	{ XK_Return,      advance,        {.i = +1} },
-	{ XK_space,       advance,        {.i = +1} },
-	{ XK_BackSpace,   advance,        {.i = -1} },
-	{ XK_l,           advance,        {.i = +1} },
-	{ XK_h,           advance,        {.i = -1} },
-	{ XK_j,           advance,        {.i = +1} },
-	{ XK_k,           advance,        {.i = -1} },
-	{ XK_Down,        advance,        {.i = +1} },
-	{ XK_Up,          advance,        {.i = -1} },
-	{ XK_Next,        advance,        {.i = +1} },
-	{ XK_Prior,       advance,        {.i = -1} },
-	{ XK_n,           advance,        {.i = +1} },
-	{ XK_p,           advance,        {.i = -1} },
-	{ XK_r,           reload,         {0} },
+    /* keysym         function        argument */
+    { XK_Escape,      quit,           {0} },
+    { XK_q,           quit,           {0} },
+    { XK_Right,       advance,        {.i = +1} },
+    { XK_Left,        advance,        {.i = -1} },
+    { XK_Return,      advance,        {.i = +1} },
+    { XK_space,       advance,        {.i = +1} },
+    { XK_BackSpace,   advance,        {.i = -1} },
+    { XK_l,           advance,        {.i = +1} },
+    { XK_h,           advance,        {.i = -1} },
+    { XK_j,           advance,        {.i = +1} },
+    { XK_k,           advance,        {.i = -1} },
+    { XK_Down,        advance,        {.i = +1} },
+    { XK_Up,          advance,        {.i = -1} },
+    { XK_Next,        advance,        {.i = +1} },
+    { XK_Prior,       advance,        {.i = -1} },
+    { XK_n,           advance,        {.i = +1} },
+    { XK_p,           advance,        {.i = -1} },
+    { XK_r,           reload,         {0} },
 };
 
 static Filter filters[] = {
-	{ "\\.ff$", "cat" },
-	{ "\\.ff.bz2$", "bunzip2" },
-	{ "\\.[a-z0-9]+$", "2ff" },
+    { "\\.ff$", "cat" },
+    { "\\.ff.bz2$", "bunzip2" },
+    { "\\.[a-z0-9]+$", "2ff" },
 };
